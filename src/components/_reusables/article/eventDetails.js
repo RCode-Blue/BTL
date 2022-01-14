a068bfd4edb951964b17c9664e988d4b9950e365
Ricky <rickyliew@hotmail.com>
2022-01-14 16:08:20 +1100

    eventDetails options

--- src/components/_reusables/article/eventDetails.js
+++ src/components/_reusables/article/eventDetails.js
@@ -11,7 +11,23 @@ const EventDetails = (data) => {
   const Bold = ({ children }) => <span className="bold">{children}</span>;
   const Text = ({ children }) => <p className="align-center">{children}</p>;
 
-  const options = {};
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>,
+      [BLOCKS.HEADING_3]: (node, children) => <h3>{children}</h3>,
+      [BLOCKS.HEADING_4]: (node, children) => <h4>{children}</h4>,
+      [BLOCKS.HEADING_5]: (node, children) => <h5>{children}</h5>,
+      [BLOCKS.HEADING_6]: (node, children) => <h6>{children}</h6>,
+      [BLOCKS.UL_LIST]: (node, children) => <ul>{children}</ul>,
+      [BLOCKS.OL_LIST]: (node, children) => <ol>{children}</ol>,
+      [BLOCKS.LIST_ITEM]: (node, children) => <li>{children}</li>,
+      [BLOCKS.HR]: (node, children) => <hr />,
+    },
+    renderMark: {
+      [BLOCKS.BOLD]: (node, children) => <b>{children}</b>,
+      [BLOCKS.ITALIC]: (node, children) => <i>{children}</i>,
+    },
+  };
 
   return <div className="">{renderRichText(data.data.raw, options)}</div>;
 };
