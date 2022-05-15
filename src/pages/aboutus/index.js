4ef3b5b324b163a65e41c1973d5d88864f38b9fd
Ricky <rickyliew@hotmail.com>
2022-05-16 09:58:05 +1000

    add style import to sub-pages

--- src/pages/aboutus/index.js
+++ src/pages/aboutus/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import MainLayout from "../../layouts/mainLayout";
 import AboutUsMain from "../../components/aboutUs/aboutUsMain";
+import "../../styles/allStyles.scss";
 
 const AboutUs = () => {
   return (
